09b824169f60f71a7df985e91dd81195010027d5
EddoCIP <eddocareera.i.p@gmail.com>
2022-04-29 03:01:36 +0700

    Initial Commit

---
+++ ExerciseSwiftComponent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExerciseSwiftComponent
+//
+//  Created by Eddo Careera Iriyanto Putra on 29/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExerciseSwiftComponent/ExerciseSwiftComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseSwiftComponentApp.swift
+//  ExerciseSwiftComponent
+//
+//  Created by Eddo Careera Iriyanto Putra on 29/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseSwiftComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
